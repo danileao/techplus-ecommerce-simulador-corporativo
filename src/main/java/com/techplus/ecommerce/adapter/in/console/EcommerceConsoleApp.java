12a1e8f47038fd6ad7d2badbbd4504833d470fa0
Daniele <dani.leao89@gmail.com>
2025-08-22 22:07:01 -0300

    simulando conflito1

--- src/main/java/com/techplus/ecommerce/adapter/in/console/EcommerceConsoleApp.java
+++ src/main/java/com/techplus/ecommerce/adapter/in/console/EcommerceConsoleApp.java
@@ -13,17 +13,17 @@ import java.time.LocalDate;
 public class EcommerceConsoleApp {
 
     public static void main(String[] args) {
-        var productRepository = new ProductRepositoryInMemory();
-
-        productRepository.save(new Product("Notebook", 3500, true));
-        productRepository.save(new Product("Mouse", 150, true));
-
-        var orderService = new OrderService(productRepository);
+        var productRepo = new ProductRepositoryInMemory();
+        var orderService = new OrderService(productRepo);
 
         Customer customer = new Customer("Dani", true);
         Order order = new Order(customer);
+        productRepo.save(new Product("Produto de Notebook", 3500, true));
+        System.out.println("----------");
+        productRepo.save(new Product("Mouse", 150, true));
 
-        orderService.addItem(order, "Notebook", 1);
+
+        orderService.addItem(order, "Produto de Notebook", 1);
         orderService.addItem(order, "Mouse", 2);
 
         VoucherDiscount voucher = new VoucherDiscount("PROMO20", 20, LocalDate.now().plusDays(5));
